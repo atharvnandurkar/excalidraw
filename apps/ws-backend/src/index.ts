b6e3b411eecaa27608b4b18f88ab97b80718d8b1
Atharva Nandurkar <devatharv@gmail.com>
2026-06-02 21:22:00 +0530

    write bussiness logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,27 @@
 import { WebSocketServer } from 'ws';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
 
 const wss = new WebSocketServer({port: 8080});
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+    // whenever someone interact with your ws server, you get ws objext to interact with them.
+    // to send them msg event. 
+    // ["ws://localhost:3000", "token=123123"]
+    const url = request.url;
+    if(!url) {
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') || '';
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    // @ts-ignore
+    if(!decoded || !(decoded as JwtPayload).userId) {
+        ws.close();
+        return;
+    }
 
     ws.on('message', function message(data) {
         ws.send('pong');
